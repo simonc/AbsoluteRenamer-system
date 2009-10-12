e7848f25fe6c6b172ad4c7c820d5d8368b0be48c
Simon COURTOIS <happynoff@free.fr>
2009-10-12 02:16:53 +0200

    Adding the sys action

--- lib/AbsoluteRenamer-system/module.rb
+++ lib/AbsoluteRenamer-system/module.rb
@@ -3,14 +3,17 @@ module AbsoluteRenamer
         def initialize
             @filters = [
                 pattern('env:(\w+)'),
+                pattern('sys:(uname( -[imnoprsv]+)?|hostname( -s)?|whoami|id -[gnru]+)')
             ]
 
             @slash_replacement = conf[:options][:system_slash_replacement] || '-'
         end
 
         def interpret(file, infos, type)
-            if infos[0].match(/env:(\w+)/)
+            if infos[0].match(/env:/)
                 self.env(infos)
+            elsif infos[0].match(/sys:/)
+                self.sys(infos)
             end
         end
 
@@ -21,6 +24,15 @@ module AbsoluteRenamer
             val = val.gsub(/\//, @slash_replacement)
 
             modify val, infos[2]
+        end
+
+        def sys(infos)
+            modifier = infos.delete_at(5)
+            infos.compact!
+
+            val = `#{infos[2]}`
+
+            modify val.chomp!, modifier
         end
     end
 end
